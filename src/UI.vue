42a9413431fa8f41080796ad60c09fbba59e2c67
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-16 12:22:24 +0100

    hooks update

--- src/UI.vue
+++ src/UI.vue
@@ -2,7 +2,27 @@
     <MyHeader />
     <div class="page">
       <MyNav />
-      <main>Contenu de la page</main>
+      <main>
+        
+        Contenu de la page
+    <p v-if="ok"> Contenu affiché si ok est vrai </p>
+    <p v-else> Contenu affiché si ok est faux </p>
+    <p v-show="ok"> Contenu affiché si ok est vrai  en v show</p>
+    <p v-show="!ok"> Contenu affiché si ok est faux en v show</p>
+    <ul>
+        <li v-for="i in 10" :key="i">{{ i }}</li>
+    </ul>
+    <span v-for="(i, j) in [3,5,7,9,2,8,3]" :key="j"> Valeur de i: {{ i }} ayant l'index {{ j }} <br></span>
+    <button v-on:click.once="maFonction">
+        Cliquez ici
+    </button>
+
+    <button @click="maFonction">
+        ou cliquez la 
+    </button>
+    <div @click.left="lclicl" @click.right.prevent="rclic" @keypress.up="keyup">Une souris verte</div>
+
+    </main>
     </div>
     <MyFooter />
   </template>
@@ -18,6 +38,25 @@
   
   export default({
     name: 'UI',
+    data() {
+      return {
+        ok: true
+      }
+    },
+    methods: {
+      maFonction() {
+        alert('Vous avez cliqué');
+      },
+        lclicl() {
+            alert('Vous avez cliqué sur la souris gauche');
+        },
+        rclic() {
+            alert('Vous avez cliqué sur la souris droite');
+        },
+        keyup() {
+            alert('Vous avez cliqué sur la touche haut');
+        }
+    },
     components: { MyHeader, MyNav, MyFooter}
   });
   </script>
