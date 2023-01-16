fe12d8adb1db47004df9d33364c348013fcb7091
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-16 15:12:03 +0100

    add ordersslot + debut tableorders

--- src/UI.vue
+++ src/UI.vue
@@ -1,75 +1,124 @@
 <template>
-    <MyHeader :titre="titre" :statut="open" @ouvrir="ouvrir"/>
-    <div class="page">
-      <MyNav  :statut="open" @chgTitre="chgTitre"/>
-      <main>
-        
-        Contenu de la page
-    <p v-if="ok"> Contenu affiché si ok est vrai </p>
-    <p v-else> Contenu affiché si ok est faux </p>
-    <p v-show="ok"> Contenu affiché si ok est vrai  en v show</p>
-    <p v-show="!ok"> Contenu affiché si ok est faux en v show</p>
-    <ul>
+  <MyHeader v-bind:titre="titre" :statut="open" @ouvrir="ouvrir" />
+  <div class="page">
+    <MyNav :statut="open" @chgTitre="chgTitre" />
+    <main>
+      Contenu de la page
+      <p v-if="ok">Contenu affiché si ok est vraie</p>
+      <p v-else>Contenu affiché si ok est fausse</p>
+      <p v-show="ok">Contenu affiché si ok est vraie, sinon display: none</p>
+      <p v-show="!ok">Contenu afiché si ok est fausse, sinon display: none</p>
+      <ul>
         <li v-for="i in 10" :key="i">{{ i }}</li>
-    </ul>
-    <span v-for="(i, j) in [3,5,7,9,2,8,3]" :key="j"> Valeur de i: {{ i }} ayant l'index {{ j }} <br></span>
-    <button v-on:click.once="maFonction">
-        Cliquez ici
-    </button>
+      </ul>
+      <span v-for="(i, j) in [3,5,7,9,2,8,3]" :key="j">
+        Valeur de i: {{ i }} ayant l'index: {{ j }}<br />
+      </span>
+      <span v-for="(i, j) in tableau" :key="j">
+        Valeur de i: {{ i }} ayant l'index: {{ j }}<br />
+      </span>
+      <button v-on:click.once="maFonction">Cliquez ici</button>
+      <button @click="maFonction">ou cliquez là</button>
+      <div @click.left="lclic" @click.prevent.right="rclic">Une souris verte</div>
 
-    <button @click="maFonction">
-        ou cliquez la 
-    </button>
-    <div @click.left="lclicl" @click.right.prevent="rclic" @keypress.up="keyup">Une souris verte</div>
-
+      <OrdersSlot>{{ orders.length }}</OrdersSlot>
+      <TableOrders :orders="orders" :headers="headers" />
     </main>
-    </div>
-    <MyFooter />
-  </template>
-  
-  <script>
-  // le template => interface affichée dans le navigateur 
-  // script => variables, évènements, hooks, ...
-  //style => mise en forme CSS. Si "scoped" uniquement le composant 
-  //import des composants + déclaration dans 'components'
-  import MyHeader from './components/MyHeader.vue';
-  import MyFooter from './components/MyFooter.vue';
-  import MyNav from './components/MyNav.vue';
-  
-  export default({
-    name: 'UI',
-    data() {
-      return {
-        ok: true,
-        open: true,
-        titre: 'Mon CRM',
-      }
+  </div>
+  <MyFooter />
+</template>
+
+<script>
+// le template => interface affichée dans le navigateur 
+// script => variables, évènements, hooks, ...
+//style => mise en forme CSS. Si "scoped" uniquement le composant 
+//import des composants + déclaration dans 'components'
+import MyHeader from './components/MyHeader.vue';
+import MyFooter from './components/MyFooter.vue';
+import MyNav from './components/MyNav.vue';
+import OrdersSlot from './components/OrdersSlot.vue';
+import TableOrders from './components/TableOrders.vue';
+
+export default({
+  name: 'UI',
+  data() {
+    return {
+      ok: false,
+      open: open,
+      titre: 'Mon CRM',
+      tableau: [3,5,7,9,2,8,6,0],
+      headers: [
+        'Actions',
+        'Type',
+        'Client',
+        'Taux TVA',
+        'Nb Jours',
+        'TJM HT',
+        'Total HT',
+        'Total TTC',
+        'Commentaire',
+        'Statut'
+      ],
+      orders: [
+        {
+          tjmHt: 1200,
+          nbJours: 100,
+          tva: 20,
+          state: 'CONFIRMED',
+          typePresta: 'Formation',
+          client: 'M2I',
+          comment: 'Merci pour la commande',
+          id: 1
+        },
+        {
+          tjmHt: 1000,
+          nbJours: 20,
+          tva: 20,
+          state: 'CANCELED',
+          typePresta: 'Coaching',
+          client: 'MI2',
+          comment: 'Oups',
+          id: 2
+        },
+        {
+          tjmHt: 1300,
+          nbJours: 10,
+          tva: 20,
+          state: 'OPTION',
+          typePresta: 'Formation',
+          client: 'MI6',
+          comment: 'En attente de la commande',
+          id: 3
+        }
+      ]
+    }
+  },
+  // methods contient nos propres fonctions
+  methods: {
+    maFonction() {
+      alert('ok');
     },
-    methods: {
-      maFonction() {
-        alert('Vous avez cliqué');
-      },
-        lclicl() {
-            alert('Vous avez cliqué sur la souris gauche');
-        },
-        rclic() {
-            alert('Vous avez cliqué sur la souris droite');
-        },
-        keyup() {
-            alert('Vous avez cliqué sur la touche haut');
-        },
-      ouvrir() {
-        this.open = !this.open;
-      },
-      chgTitre(titre) {
-        this.titre = "mon nouveau titre";
-      }
+    lclic() {
+      console.log('clic gauche');
     },
-    components: { MyHeader, MyNav, MyFooter}
-  });
-  </script>
-  
-  <style scoped>
-  
-  
-  </style>
+    rclic() {
+      //utilisation de prevent pour éviter l'affichage du menu contextuel
+      console.log('clic droit');
+    },
+    ouvrir() {
+      this.open = !this.open;
+    },
+    // la demande de changement de titre vient du composant enfant <MyNav />
+    chgTitre() {
+      this.titre = "Mon nouveau titre";
+    }
+  },
+  // déclaration des composants affichés sur la page
+  components: { MyHeader, MyNav, MyFooter, OrdersSlot, TableOrders }
+});
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/TableOrders.vue
@@ -0,0 +1,34 @@
+<template>
+<table>
+    <tr>
+        <th v-for="(head, i) in headers" :key="i">{{ head }}</th>
+    </tr>
+    <!-- affichage des orders -->
+    
+</table>
+
+</template>
+
+<script>
+export default ({
+    name: 'TableOrders',
+    props: {
+        orders: {
+            type: Object,
+        },
+        headers: {
+            type: Object,
+        }
+    },
+})
+</script>
+
+<style scoped>
+table{
+    
+    width: 90%;
+    margin: 40px auto;
+    border: 1px solid silver;
+    border-collapse: collapse;
+}
+</style>
